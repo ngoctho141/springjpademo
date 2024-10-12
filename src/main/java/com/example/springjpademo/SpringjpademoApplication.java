672bf006fc3adaa199ac675f4312f470029326dd
Ngoc Tho Nguyen <thofreising141@gmail.com>
2024-10-12 15:16:47 +0000

    add dev container

--- src/main/java/com/example/springjpademo/SpringjpademoApplication.java
+++ src/main/java/com/example/springjpademo/SpringjpademoApplication.java
@@ -1,7 +1,12 @@
 package com.example.springjpademo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.springjpademo.entity.Student;
+import com.example.springjpademo.repository.StudentRepository;
 
 @SpringBootApplication
 public class SpringjpademoApplication {
@@ -10,10 +15,10 @@ public class SpringjpademoApplication {
         SpringApplication.run(SpringjpademoApplication.class, args);
     }
 
-//	 @Bean
-//	 CommandLineRunner initDatabase(StudentRepository repository) {
-//	 	return args -> repository
-//	 			.save(Student.builder().emailId("thonn@gmail.com").firstName("Nguyen").lastName("Ngoc Tho").build());
-//	 }
+	//  @Bean
+	//  CommandLineRunner initDatabase(StudentRepository repository) {
+	//  	return args -> repository
+	//  			.save(Student.builder().emailId("thonn@gmail.com").firstName("Nguyen").lastName("Ngoc Tho").build());
+	//  }
 
 }
---
+++ src/main/java/com/example/springjpademo/controller/GreetingController.java
@@ -0,0 +1,8 @@
+package com.example.springjpademo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class GreetingController {
+    
+}
